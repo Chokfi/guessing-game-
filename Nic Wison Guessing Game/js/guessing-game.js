771c4e59c229b8b0acbc16e85230a556ba98d670
Nicholas Wilson <69205862+AatiImilillaIkso@users.noreply.github.com>
2022-09-04 23:30:58 -0500

    Add files via upload

---
+++ Nic Wison Guessing Game/js/guessing-game.js
@@ -0,0 +1,12 @@
+let btn= document.getElementById('btn')
+let output = docuemnt.getElementbyId('outputText')
+
+let number=[Math.floor(Math.random( )*100)]
+
+btn.addEventListener("click". function())
+let input=  textbox.getElementById("userInput").Value;
+if(input==number)
+output.innerHTM= you guessed the right number
+} Else if (input <number) 
+;
+
